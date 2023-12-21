2f0b0159592fbc1a248bb3d46fbd6b137cab7704
louie <louiecasula@gmail.com>
2023-12-21 15:59:45 -0500

    Set up Spring files

---
+++ src/main/java/com/passion/lingosphere/LingosphereApplication.java
@@ -0,0 +1,13 @@
+package com.passion.lingosphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingosphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingosphereApplication.class, args);
+	}
+
+}
