a7119f93aa66bb0611343fb425501cb489b1d6b1
louie <louiecasula@gmail.com>
2023-12-22 09:01:53 -0500

    Words are saved in the WordRepository via WordConfig

--- src/main/java/com/passion/lingosphere/word/WordController.java
+++ src/main/java/com/passion/lingosphere/word/WordController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/word")
+@RequestMapping(path = "/word")
 public class WordController {
 
     private final WordService wordService;
