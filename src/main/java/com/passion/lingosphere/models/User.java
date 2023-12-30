3a6af992d5da29374dd32fe4d14f8a049c77695c
louie <louiecasula@gmail.com>
2023-12-29 19:25:12 -0500

    added User model

---
+++ src/main/java/com/passion/lingosphere/models/User.java
@@ -0,0 +1,80 @@
+package com.passion.lingosphere.models;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UserLanguage> userLanguages = new HashSet<>();
+
+    // Constructors
+    public User() {
+        // Default constructor
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<UserLanguage> getUserLanguages() {
+        return userLanguages;
+    }
+
+    public void setUserLanguages(Set<UserLanguage> userLanguages) {
+        this.userLanguages = userLanguages;
+    }
+
+    // Additional methods like equals(), hashCode(), and toString() can be added as needed
+}
