ddd701f92c161c8c43bd2de1bc01c31effe16c9f
louie <louiecasula@gmail.com>
2023-12-29 19:41:35 -0500

    Added repositories for each new entity

---
+++ src/main/java/com/passion/lingosphere/repositories/UserLanguageRepository.java
@@ -0,0 +1,9 @@
+package com.passion.lingosphere.repositories;
+
+import com.passion.lingosphere.models.UserLanguage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserLanguageRepository extends JpaRepository<UserLanguage, Long> {
+}
