f3d525cf92a1c6eb2adef01c22c5319c06505a9d
louie <louiecasula@gmail.com>
2023-12-31 15:30:00 -0500

    Added getUserLanguages and test. Updated UserLanguageRepository accordingly.

--- src/main/java/com/passion/lingosphere/repositories/UserLanguageRepository.java
+++ src/main/java/com/passion/lingosphere/repositories/UserLanguageRepository.java
@@ -4,7 +4,11 @@ import com.passion.lingosphere.models.UserLanguage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserLanguageRepository extends JpaRepository<UserLanguage, Long> {
     boolean existsByUserIdAndLanguageId(Long userId, Long languageId);
+
+    List<UserLanguage> findByUserId(Long userId);
 }
