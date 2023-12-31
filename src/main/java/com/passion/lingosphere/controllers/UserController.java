70cb2171dd01ef5588f613fcf1c2823a2ada20c4
louie <louiecasula@gmail.com>
2023-12-31 18:47:43 -0500

    Created UserController class

---
+++ src/main/java/com/passion/lingosphere/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.passion.lingosphere.controllers;
+
+import com.passion.lingosphere.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
