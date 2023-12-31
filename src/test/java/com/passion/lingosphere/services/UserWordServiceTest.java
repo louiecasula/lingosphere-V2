4383813739f6d1e540080af206897e0f8bcd6070
louie <louiecasula@gmail.com>
2023-12-31 16:43:33 -0500

    addUserWord tested

--- src/test/java/com/passion/lingosphere/services/UserWordServiceTest.java
+++ src/test/java/com/passion/lingosphere/services/UserWordServiceTest.java
@@ -1,14 +1,20 @@
 package com.passion.lingosphere.services;
 
 import com.passion.lingosphere.models.User;
+import com.passion.lingosphere.models.UserWord;
 import com.passion.lingosphere.models.Word;
+import com.passion.lingosphere.repositories.UserRepository;
 import com.passion.lingosphere.repositories.UserWordRepository;
+import com.passion.lingosphere.repositories.WordRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
@@ -19,6 +25,10 @@ public class UserWordServiceTest {
     @Mock
     private UserWordRepository userWordRepository;
     @Mock
+    private UserRepository userRepository;
+    @Mock
+    private WordRepository wordRepository;
+    @Mock
     private User user;
     @Mock
     private Word word;
@@ -27,7 +37,15 @@ public class UserWordServiceTest {
 
     @Test
     public void addUserWordSuccessfulTest() {
+        given(userRepository.findById(0L)).willReturn(Optional.of(user));
+        given(wordRepository.findById(0L)).willReturn(Optional.of(word));
+        given(userWordRepository.save(any(UserWord.class))).willReturn(new UserWord(user, word, new Date()));
 
+        UserWord added = userWordService.addUserWord(0L, 0L);
+
+        assertNotNull(added);
+        assertEquals(user, added.getUser());
+        assertEquals(word, added.getWord());
     }
 
     @Test
