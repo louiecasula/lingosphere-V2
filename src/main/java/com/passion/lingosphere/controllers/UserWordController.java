6da8823455999989cf62fa38915c8cfe673f605b
louie <louiecasula@gmail.com>
2023-12-31 23:44:54 -0500

    Added add and get methods in UserWordController.

--- src/main/java/com/passion/lingosphere/controllers/UserWordController.java
+++ src/main/java/com/passion/lingosphere/controllers/UserWordController.java
@@ -1,9 +1,14 @@
 package com.passion.lingosphere.controllers;
 
+import com.passion.lingosphere.models.UserWord;
 import com.passion.lingosphere.services.UserWordService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users/{userId}/words")
@@ -14,5 +19,24 @@ public class UserWordController {
     @Autowired
     public UserWordController(UserWordService userWordService) {
         this.userWordService = userWordService;
+    }
+
+    @PostMapping("/{userId}/words/{wordId}")
+    public ResponseEntity<?> addUserWord(@PathVariable Long userId, @PathVariable Long wordId) {
+        try {
+            UserWord newUserWord = userWordService.addUserWord(userId, wordId);
+            return new ResponseEntity<>(newUserWord, HttpStatus.CREATED);
+        } catch (DataIntegrityViolationException e) {
+            return new ResponseEntity<>("User word creation failed: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping
+    public ResponseEntity<?> getUserWords(@PathVariable Long userId) {
+        List<UserWord> userWordList = userWordService.getUserWords(userId);
+        return new ResponseEntity<>(userWordList, HttpStatus.OK);
     }
 }
