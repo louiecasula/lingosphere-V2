f1b29e407548c6d56e6fdf559bf4973ac3ad7fef
louie <louiecasula@gmail.com>
2023-12-31 23:26:01 -0500

    Added update and remove in UserLanguageController.

--- src/main/java/com/passion/lingosphere/controllers/UserLanguageController.java
+++ src/main/java/com/passion/lingosphere/controllers/UserLanguageController.java
@@ -4,6 +4,7 @@ import com.passion.lingosphere.dtos.UserLanguageDto;
 import com.passion.lingosphere.models.UserLanguage;
 import com.passion.lingosphere.models.Word;
 import com.passion.lingosphere.services.UserLanguageService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -39,5 +40,29 @@ public class UserLanguageController {
     public ResponseEntity<?> getUserLanguages(Long userId) {
         List<UserLanguage> userLanguageList = userLanguageService.getUserLanguages(userId);
         return new ResponseEntity<>(userLanguageList, HttpStatus.OK);
+    }
+
+    @PutMapping("/{userId}/languages/{languageId}")
+    public ResponseEntity<?> updateUserLanguage(@PathVariable Long userId, @PathVariable Long languageId, @RequestBody UserLanguageDto userLanguageDto) {
+        try {
+            UserLanguage updatedUserLanguage = userLanguageService.updateUserLanguage(userId, languageId, userLanguageDto);
+            return ResponseEntity.ok(updatedUserLanguage);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{userId}/languages/{languageId}")
+    public ResponseEntity<?> removeUserLanguage(@PathVariable Long userId, @PathVariable Long languageId) {
+        try {
+            userLanguageService.removeUserLanguage(userId, languageId);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
