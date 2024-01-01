f954d2b3921d1e8e412f8367352744819f42bccd
louie <louiecasula@gmail.com>
2023-12-31 21:16:49 -0500

    Added getAllLanguages in LanguageController.

--- src/main/java/com/passion/lingosphere/controllers/LanguageController.java
+++ src/main/java/com/passion/lingosphere/controllers/LanguageController.java
@@ -7,10 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/languages")
@@ -33,5 +32,11 @@ public class LanguageController {
         } catch (Exception e) {
             return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllLanguages() {
+        List<Language> languageList = languageService.getAllLanguages();
+        return new ResponseEntity<>(languageList, HttpStatus.OK);
     }
 }
