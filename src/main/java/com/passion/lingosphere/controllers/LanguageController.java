607589ee162b0346a17ce374a5dd439dce08d649
louie <louiecasula@gmail.com>
2023-12-31 21:09:07 -0500

    Added addLanguage to LanguageController

--- src/main/java/com/passion/lingosphere/controllers/LanguageController.java
+++ src/main/java/com/passion/lingosphere/controllers/LanguageController.java
@@ -1,7 +1,14 @@
 package com.passion.lingosphere.controllers;
 
+import com.passion.lingosphere.dtos.LanguageDto;
+import com.passion.lingosphere.models.Language;
 import com.passion.lingosphere.services.LanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +21,17 @@ public class LanguageController {
     @Autowired
     public LanguageController(LanguageService languageService) {
         this.languageService = languageService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addLanguage(@RequestBody LanguageDto languageDto) {
+        try {
+            Language newLanguage = languageService.addLanguage(languageDto);
+            return new ResponseEntity<>(newLanguage, HttpStatus.CREATED);
+        } catch (DataIntegrityViolationException e) {
+            return new ResponseEntity<>("Language creation failed: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
