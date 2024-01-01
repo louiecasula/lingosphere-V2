3c174b88588bca39d51fc53df37df01afe957096
louie <louiecasula@gmail.com>
2023-12-31 21:03:26 -0500

    Created LanguageController

---
+++ src/main/java/com/passion/lingosphere/controllers/LanguageController.java
@@ -0,0 +1,18 @@
+package com.passion.lingosphere.controllers;
+
+import com.passion.lingosphere.services.LanguageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/languages")
+public class LanguageController {
+
+    private final LanguageService languageService;
+
+    @Autowired
+    public LanguageController(LanguageService languageService) {
+        this.languageService = languageService;
+    }
+}
