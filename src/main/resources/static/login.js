fd50ff708b117719eba8b8364d81818c7ea7a0f0
louie <louiecasula@gmail.com>
2024-01-03 13:19:36 -0500

    reverted changes to login.js.

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -1,32 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('login-form').addEventListener('submit', function (event) {
-        event.preventDefault();
+document.getElementById('login-form').addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        const userData = {
-            username: document.getElementById('username').value,
-            password: document.getElementById('password').value
-        };
+    const userData = {
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value
+    };
 
-        fetch('http://localhost:8080/api/users/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userData)
+    fetch('http://localhost:8080/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            if (data.userId) {
+                sessionStorage.setItem('userId', data.userId);
+            }
+
+            // Redirect to dashboard
+            window.location.href = '/dashboard.html';
         })
-            .then(response => response.json())
-            .then(userData => {
-                console.log('Success:', userData);
-                if (userData.userId) {
-                    sessionStorage.setItem('userId', userData.userId);
-                }
-
-                // Redirect to dashboard
-                window.location.href = '/dashboard.html';
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                // TODO: Send an error message to the user
-            });
-    });
+        .catch((error) => {
+            console.error('Error:', error);
+            // TODO: Send an error message to the user
+        });
 });
