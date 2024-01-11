dfc99fcc6826ed91fb14cea226bf8be142f4f60c
louie <louiecasula@gmail.com>
2024-01-10 20:31:50 -0500

    Fixed sessionStorage issue. Changed port in applications.properties to 5433.

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -1,30 +1,37 @@
-document.getElementById('login-form').addEventListener("submit", function (event) {
-    event.preventDefault();
+window.onload = function () {
+    document.getElementById('login-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const userData = document.getElementById('login-form');
+        const username = userData.username.value;
+        const password = userData.password.value;
+        console.log({username, password});
 
-    const userData = {
-        username: document.getElementById('username').value,
-        password: document.getElementById('password').value
-    };
+        const user = {
+            username: username,
+            password: password
+        };
 
-    fetch('http://localhost:8080/api/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(userData)
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            if (data.userId) {
-                sessionStorage.setItem('userId', data.userId);
-            }
-
-            // Redirect to dashboard
-            window.location.href = '/dashboard.html';
+        fetch('http://localhost:8080/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
         })
-        .catch((error) => {
-            console.error('Error:', error);
-            // TODO: Send an error message to the user
-        });
-});
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                if (data.userId) {
+                    sessionStorage.setItem('userId', data.userId);
+                    console.log('Stored username')
+                    // Redirect to dashboard
+                    window.location.href = '/dashboard.html';
+                }
+
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                // TODO: Send an error message to the user
+            });
+    });
+};
