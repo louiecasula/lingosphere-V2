e4a3cf3b27b9eaf3762530fd6ec3812fd9185242
louie <louiecasula@gmail.com>
2024-02-04 13:08:19 -0500

    Added React file and Navbar component. Removed prior html and js files.

--- src/main/resources/static/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('signup-btn').addEventListener('click', function() {
-    window.location.href = 'signup.html';
-});
-
-document.getElementById('signin-btn').addEventListener('click', function() {
-    window.location.href = 'login.html';
-});
