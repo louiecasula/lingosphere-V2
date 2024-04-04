26e48f8f26e544e4995902ac5e30f21e22e2c676
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-04 16:44:27 -0400

    Fixed routes (for real this time).

--- lingosphere-frontend/src/index.js
+++ lingosphere-frontend/src/index.js
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Layout from './pages/Layout';
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate, Outlet} from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import LangSelect from "./pages/LangSelect";
 import Profile from "./pages/Profile";
 import Dashboard from "./pages/Dashboard";
 
-const ProtectedRoute = ({ children }) => {
-    if (!sessionStorage.getItem("userId")) {
-        // Redirect to the sign-in page
-        return <Navigate to="/signin" />;
-    }
-    return children;
+// Redirect user if they aren't signed in
+const ProtectedRoute = () => {
+    return sessionStorage.getItem("userId")? <Outlet />: <Navigate to="/signin" />;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -24,12 +21,13 @@ root.render(
             <Routes>
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/signin" element={<SignIn />} />
-                <Route element={<ProtectedRoute />} />
+                <Route element={<ProtectedRoute />}>
                     <Route element={<Layout />}>
                         <Route path="/profile" element={<Profile />} />
                         <Route path="/" element={<Dashboard />} />
                         <Route path="/language-select" element={<LangSelect />} />
                     </Route>
+                </Route>
             </Routes>
         </Router>
     </React.StrictMode>
--- lingosphere-frontend/src/pages/Layout.js
+++ lingosphere-frontend/src/pages/Layout.js
@@ -1,14 +1,17 @@
 import './Layout.css';
 import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom';
 
 console.log(sessionStorage.getItem('userId'));
 console.log(sessionStorage.getItem('username'));
 
-const Layout = ({children}) => (
-  <>
-    <Navbar />
-    {children}
-  </>
-);
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  )
+};
 
 export default Layout;
