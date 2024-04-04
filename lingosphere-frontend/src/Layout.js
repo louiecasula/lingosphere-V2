2e5d9a0af9975112885b4ab306b1fc4da0fea400
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-04 16:12:10 -0400

    Added Layout to hold all template components.

---
+++ lingosphere-frontend/src/Layout.js
@@ -0,0 +1,14 @@
+import './Layout.css';
+import Navbar from './components/Navbar'
+
+console.log(sessionStorage.getItem('userId'));
+console.log(sessionStorage.getItem('username'));
+
+const Layout = ({children}) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+);
+
+export default Layout;
