e9b8cfcde05dea3dc609f9003a2dcbca8e5a987d
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-05 14:53:47 -0400

    Added pseudocode for the word-of-the-day functionality.

--- lingosphere-frontend/src/pages/Dashboard.js
+++ lingosphere-frontend/src/pages/Dashboard.js
@@ -1,3 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { getWordsOfTheDay } from '../api/wordApi';
 
-export default function Dashboard() {};
+export default function Dashboard() {
+    const [words, setWords] = useState([]);
+
+    //// Word-of-the-day function ////
+    // *Might have to double check that the service logic makes sense...
+    // Check if words have already been assigned for the day in each language
+    // Display the words
+
+    /* useEffect(() => {
+        const userId = sessionStorage.getItem('userId');
+        getWordsOfTheDay({userId}).then(wordsFetched => {
+            setWords(wordsFetched);
+        }).catch(error => console.error(error));    
+    }, []); */
+
+    /* return (
+        <div>
+            {words.map((word, index) => (
+                <div key={index}>{word}</div>
+            ))}
+        </div>
+    ); */
+}; 
