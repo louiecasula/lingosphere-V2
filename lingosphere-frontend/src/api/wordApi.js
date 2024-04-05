52f0e12ddb810cc1ad0cc3c30b4201209dc18b2e
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-05 14:54:26 -0400

    Added getWordsOfTheDay fetch function.

---
+++ lingosphere-frontend/src/api/wordApi.js
@@ -0,0 +1,24 @@
+const API_BASE_URL = 'http://localhost:8080/api/words';
+
+export function getWordsOfTheDay(userData) {
+    const { userId } = userData;
+
+    return fetch(`${API_BASE_URL}/${userId}/words-of-the-day`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(data => {
+                    throw new Error(`Server error: ${data.message || response.status}`);
+                });
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.error('Error fetching word(s)-of-the-day for user:', error);
+            throw error;
+        });
+}
