ff7e2aa0945933c8b4c67a843f421a14e767ec60
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-05 10:32:09 -0400

    Added getUserWords call to Profile page.

--- lingosphere-frontend/src/pages/Profile.js
+++ lingosphere-frontend/src/pages/Profile.js
@@ -1,3 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { getUserWords } from '../api/userWordApi';
 
-export default function Profile() {};
+export default function Profile() {
+    const [words, setWords] = useState([]);
+
+    useEffect(() => {
+        const userId = sessionStorage.getItem('userId');
+        getUserWords({userId}).then(wordsFetched => {
+            setWords(wordsFetched);
+        }).catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            {words.map((word, index) => (
+                <div key={index}>{word}</div>
+            ))}
+        </div>
+    );
+};
