f3982dfe9c3297e56ac5d88a1b458d6f0104a194
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-05 11:03:00 -0400

    Added getUserLanguages fetch function.

--- lingosphere-frontend/src/api/userLanguageApi.js
+++ lingosphere-frontend/src/api/userLanguageApi.js
@@ -1,5 +1,28 @@
 const API_BASE_URL = 'http://localhost:8080/api/users';
 
+export function getUserLanguages(userData) {
+    const { userId } = userData;
+
+    return fetch(`${API_BASE_URL}/${userId}/languages`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(data => {
+                    throw new Error(`Server error: ${data.message || response.status}`);
+                });
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.error('Error fetching languages for user:', error);
+            throw error;
+        });
+}
+
 export function addUserLanguage(userData) {
     const { userId, languageId, proficiencyLevel } = userData;
 
