f1a53efc12da8bd423a1654471645cfa712fd6c1
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-05 10:29:39 -0400

    Fixed typo in base URL.

--- lingosphere-frontend/src/api/userLanguageApi.js
+++ lingosphere-frontend/src/api/userLanguageApi.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080/api/users/';
+const API_BASE_URL = 'http://localhost:8080/api/users';
 
 export function addUserLanguage(userData) {
     const { userId, languageId, proficiencyLevel } = userData;
