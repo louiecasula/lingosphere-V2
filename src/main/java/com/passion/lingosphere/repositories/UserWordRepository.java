a7aeb7fc1e810e5eacd32e409c0f687656dec1cf
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-07 17:22:19 -0400

    Added findByDateSent method.

--- src/main/java/com/passion/lingosphere/repositories/UserWordRepository.java
+++ src/main/java/com/passion/lingosphere/repositories/UserWordRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.time.LocalDate;
 
 @Repository
 public interface UserWordRepository extends JpaRepository<UserWord, Long> {
     List<UserWord> findByUser_UserId(Long userId);
+
+    List<UserWord> findByDateSent(LocalDate LocalDate);
 }
