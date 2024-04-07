ccbb3a6dc3eb30d1d6f8de41d38526406b2146f0
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-07 18:40:20 -0400

    Added a greeting for the user when they receive their wotd.

--- lingosphere-frontend/src/pages/Dashboard.js
+++ lingosphere-frontend/src/pages/Dashboard.js
@@ -4,6 +4,7 @@ import { generateWordsOfTheDay } from '../api/wordApi';
 
 export default function Dashboard() {
     const [words, setWords] = useState([]);
+    const username = sessionStorage.getItem('username');
 
     useEffect(() => {
         const userId = sessionStorage.getItem('userId');
@@ -13,10 +14,20 @@ export default function Dashboard() {
     }, {});
 
     // TODO: Make a component that greets the user in one of the languages they've signed up for
+    const getOrdinalSuffix = (n) => {
+        return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
+    };
+
+    const todaysDate = new Date();
+    const dayWithSuffix = getOrdinalSuffix(todaysDate.getDate());
+    const month = todaysDate.toLocaleString('default', { month: 'long' });
+    const year = todaysDate.getFullYear();
+
+    const headerTitle = `${username}'s words for ${month} ${dayWithSuffix}, ${year}:`;
 
     return (
         <>
-            <h1>Your Words of The Day</h1>
+            <h1>{headerTitle}</h1>
             <div className="wotd-container">
                 {Object.entries(words).map(([lang, word]) => (
                     <div key={lang}>
