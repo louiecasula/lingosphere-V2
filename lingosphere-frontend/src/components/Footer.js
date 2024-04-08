c308840bec6296a8b2652a10b7b17b811d8f11b0
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-08 10:53:17 -0400

    Added eventhandler for icon spin animation on hover.

--- lingosphere-frontend/src/components/Footer.js
+++ lingosphere-frontend/src/components/Footer.js
@@ -1,11 +1,25 @@
-import * as React from 'react';
+import React, { useRef } from 'react';
 import './Footer.css';
 
 function Footer() {
+    const iconRef = useRef(null);
+
+    const handleMouseEnter = () => {
+    iconRef.current.style.animationName = 'spin';
+    };
+
+    const handleMouseLeave = () => {
+    iconRef.current.style.animationName = 'spinBack';
+    };
+
     return (
         <footer>
-            <p>Copyright © 2024 The Leuth <a href="https://github.com/louiecasula" target="_blank">
-                <i class="fa fa-github" aria-hidden="true"></i></a></p>
+            <p>Copyright © 2024 The Leuth
+                <a href="https://github.com/louiecasula" target="_blank">
+                    <i ref={iconRef} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} 
+                    class="fa fa-github" aria-hidden="true"></i>
+                </a>
+            </p>
         </footer>
     );
 }
