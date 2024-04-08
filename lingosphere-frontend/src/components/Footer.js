64ef12c30dc29cce5e54466832006810cdb618b9
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-07 22:23:06 -0400

    Created a footer component.

---
+++ lingosphere-frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <p>Copyright © 2024 The Leuth <a href="https://github.com/louiecasula" target="_blank">
+                <i class="fa fa-github" aria-hidden="true"></i></a></p>
+        </footer>
+    );
+}
+export default Footer;
