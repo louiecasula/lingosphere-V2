6cf6bf4cb9a0397a6e85f170b0e36f316a635fcd
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-08 18:06:56 -0400

    Abstracted data table into its own component.

--- lingosphere-frontend/src/pages/Profile.js
+++ lingosphere-frontend/src/pages/Profile.js
@@ -1,36 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { getUserWords } from '../api/userWordApi';
+import * as React from 'react';
+import Table from '../components/Table';
 import './Profile.css';
 
 export default function Profile() {
-    const [words, setWords] = useState([]);
-
-    useEffect(() => {
-        const userId = sessionStorage.getItem('userId');
-        getUserWords({userId}).then(wordsFetched => {
-            setWords(wordsFetched);
-        }).catch(error => console.error(error));
-    }, []);
-
     return (
         <>
             <h1>Word History</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Word</th>
-                        <th>Date Received</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {words.map((word, index) => (
-                        <tr key={index}>
-                            <td>{word.word.text}</td>
-                            <td>{word.dateSent}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <Table />
         </>
     );
 };
