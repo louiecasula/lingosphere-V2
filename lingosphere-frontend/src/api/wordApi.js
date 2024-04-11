1d86dcd9ff50a0499f7c9b5a2227fec31bab1c78
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-11 12:22:42 -0400

    Added getWordById fetch function.

--- lingosphere-frontend/src/api/wordApi.js
+++ lingosphere-frontend/src/api/wordApi.js
@@ -22,3 +22,25 @@ export function generateWordsOfTheDay(userData) {
             throw error;
         });
 }
+
+export function getWordById(wordId) {
+
+    return fetch(`${API_BASE_URL}/${wordId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(data => {
+                    throw new Error(`Server error: ${data.message || response.status}`);
+                });
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.error('Error fetching word details:', error);
+            throw error;
+        });
+}
