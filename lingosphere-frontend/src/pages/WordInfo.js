e3c5cbb2537febbcbeab43d99811d7d7dd7b83a7
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-11 12:33:21 -0400

    Created WordInfo page.

---
+++ lingosphere-frontend/src/pages/WordInfo.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getWordById } from '../api/wordApi';
+
+export default function WordInfo() {
+  const { wordId } = useParams();
+  const [wordDetails, setWordDetails] = useState(null);
+
+  useEffect(() => {
+    getWordById(wordId).then(data => {
+      setWordDetails(data);
+    }).catch(error => {
+      console.error('Error fetching word details:', error);
+    });
+  }, [wordId]);
+
+  if (!wordDetails) {
+    return <div className='load-status'>Loading...</div>;
+  }
+
+  return (
+    <div className="wotd-container">
+        <div className="wotd">
+            <div className="lang">
+                <strong>{wordDetails.language.name}:</strong>
+            </div>
+            <div className="word">
+                <h1>{wordDetails.text}</h1>
+                <hr/>
+                <i>{wordDetails.partOfSpeech}pos</i>
+                <br/>
+                {wordDetails.definition}this is the definition of the word
+            </div>
+        </div>
+    </div>
+  );
+}
