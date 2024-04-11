98aeb574ecd798813ae04aced77c71bb9542c8e4
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-11 12:23:16 -0400

    Added route for WordInfo page.

--- lingosphere-frontend/src/index.js
+++ lingosphere-frontend/src/index.js
@@ -8,6 +8,7 @@ import SignIn from "./pages/SignIn";
 import LangSelect from "./pages/LangSelect";
 import Profile from "./pages/Profile";
 import Dashboard from "./pages/Dashboard";
+import WordInfo from "./pages/WordInfo";
 
 // Redirect user if they aren't signed in
 const ProtectedRoute = () => {
@@ -26,6 +27,7 @@ root.render(
                         <Route path="/word-history" element={<Profile />} />
                         <Route path="/" element={<Dashboard />} />
                         <Route path="/settings" element={<LangSelect />} />
+                        <Route path="/words/:wordId" element={<WordInfo />} />
                     </Route>
                 </Route>
             </Routes>
