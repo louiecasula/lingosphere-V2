1cf9b9242cced312b283bba7ae344070b9f25719
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-15 16:25:57 -0400

    Created Settings page, abstracted LangSelect into a component.

---
+++ lingosphere-frontend/src/pages/Settings.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import LangSelect from '../components/LangSelect';
+
+export default function Settings() {
+    return (
+        <>
+            <h1>Settings</h1>
+            <LangSelect />
+        </>
+    );
+};
