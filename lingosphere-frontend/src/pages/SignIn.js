0939aed3e802cf041964f05d421ce00c9a807e19
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-16 17:41:04 -0400

    Tidied up imports.

--- lingosphere-frontend/src/pages/SignIn.js
+++ lingosphere-frontend/src/pages/SignIn.js
@@ -1,22 +1,10 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import { Avatar, Button, CssBaseline, TextField, Link, Paper, Box,Grid, Typography, createTheme, ThemeProvider } from '@mui/material';
+// import { FormControlLabel, Checkbox } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { loginUser } from '../api/userApi';
 
 const defaultTheme = createTheme();
-
-
 
 export default function SignIn() {
   const handleSubmit = (event) => {
