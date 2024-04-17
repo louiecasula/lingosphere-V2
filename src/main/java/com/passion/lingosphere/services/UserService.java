5d2c2d1a280a25b16debe0af7379d4ac90802d0b
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-04-17 17:47:51 -0400

    Updated comments.

--- src/main/java/com/passion/lingosphere/services/UserService.java
+++ src/main/java/com/passion/lingosphere/services/UserService.java
@@ -24,33 +24,31 @@ public class UserService {
     }
 
     public User registerUser(UserDto userDto) throws UsernameAlreadyExistsException, EmailAlreadyExistsException {
-        // Check if the user already exists
+        // Check if the user already exists by username
         if (userRepository.existsByUsername(userDto.getUsername())) {
             throw new UsernameAlreadyExistsException("Username is taken");
         }
-
+        // Check if user already exists by email
         if (userRepository.existsByEmail(userDto.getEmail())) {
             throw new EmailAlreadyExistsException("Email is already in use");
         }
-
         // Create a new User entity from the DTO
         User newUser = new User();
         newUser.setUsername(userDto.getUsername());
         newUser.setEmail(userDto.getEmail());
         newUser.setPassword(passwordEncoder.encode(userDto.getPassword()));
-
         // Save the new user entity
         return userRepository.save(newUser);
     }
 
     public User authenticateUser(String email, String password) throws AuthenticationException {
-        // Check that the user's username and password exist
+        // Check that the user's email is in database
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new AuthenticationException("Email not found"));
+        // Check that the user's password is associated with their email
         if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new AuthenticationException("Invalid password");
         }
-
         // Return user
         return user;
     }
